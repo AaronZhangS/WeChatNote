a1a00720d05064c8ee3ce6ab29e2618e5a0e9fa0
zhangsheng <1164968903@qq.com>
2020-03-23 23:01:02 +0800

    添加显示用用户信信息

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -1,80 +1,106 @@
 // miniprogram/pages/home/home.js
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // 判断小程序的API，回调，参数，组件等是否在当前版本可用
+    canIUse:wx.canIUse('button.open-type.getUserInfo')
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    // 查看是否授权
+    wx.getSetting({
+      success: function (res) {
+        if (res.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success: function (res) {
+              console.log(res.userInfo)
+              //用户已经授权过
+              console.log('用户已经授权过')
+            },
+            fail:function(){
+              // 重新授权
+              
+            }
+          })
+        }
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
 
   /**
    *  用户点击事件
    */
-  onGotUserInfo: function (e) {
-    console.log(e.detail.errMsg)
-    console.log(e.detail.userInfo)
-    console.log(e.detail.userInfo.nickName)
-    console.log(e.detail.userInfo.avatarUrl)
-    console.log(e.detail.userInfo.gender)
-    console.log(e.detail.userInfo.province)
-    console.log(e.detail.userInfo.city)
-    console.log(e.detail.userInfo.country)
+  getUserInfo(e){
+    console.log(e)
   },
+
+   /**
+   *  用户授权操作
+   */
+  bindGetUserInfo: function(e) {
+    console.log('option',e.detail.userInfo)
+    if (e.detail.userInfo){
+      //用户按了允许授权按钮
+      app.globalData.authorized = true
+    } else {
+      //用户按了拒绝按钮
+      app.globalData.authorized = false
+    }
+  }
 })
